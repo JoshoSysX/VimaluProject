423e135744f8d6a1d9d0052005dadf62cc7efb33
JOSIAS <josias.guillermo@upeu.edu.pe>
2026-09-24 13:16:36 -0500

    Auth actualizado

---
+++ src/main/java/com/web/proyect/hacienda_vimalu/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.web.proyect.hacienda_vimalu.controller;
+
+import com.web.proyect.hacienda_vimalu.dto.RegistroDTO;
+import com.web.proyect.hacienda_vimalu.dto.UsuarioDTO;
+import com.web.proyect.hacienda_vimalu.services.IUsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final IUsuarioService usuarioService;
+
+    public AuthController(IUsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/registro")
+    public ResponseEntity<UsuarioDTO> registrarCliente(
+            @Valid @RequestBody RegistroDTO dto) {
+
+        return usuarioService.registrarCliente(dto)
+                .map(usuario -> ResponseEntity.status(HttpStatus.CREATED).body(usuario))
+                .orElseGet(() ->
+                        ResponseEntity.badRequest().build());
+    }
+}
